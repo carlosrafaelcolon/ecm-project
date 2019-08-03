dd4395bc727e775802353ba2ce71fb5a565ae02e
Raffy <carlosrafaelcolon@gmail.com>
2019-08-03 13:46:55 -0400

    axios fix

--- server/client/src/apis/backend.js
+++ server/client/src/apis/backend.js
@@ -3,13 +3,15 @@ import axios from 'axios';
 export const user_route = axios.create({
   baseURL: '/api/v1/user',
   headers: {
-    'Authorization': 'Bearer ' + localStorage.getItem('token')
-  }
+    'Authorization': 'Bearer ' + localStorage.getItem('token'),
+  },
+  'crossDomain': true
 })
 
 export const yt_route = axios.create({
   baseURL: '/api/v1/youtube',
   headers: {
     'Authorization': 'Bearer ' + localStorage.getItem('token')
-  }
+  },
+  'crossDomain': true
 })
